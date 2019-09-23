7328059645ee890c8bbda6fb7354cfa17ed49305
TAIKI IKEDA <taiki_ikeda@relationsgroup.co.jp>
2019-09-24 00:51:14 +0900

    Fix todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import '@fortawesome/fontawesome-free/css/all.css'
 import './Todo.css'
 
 interface TodoProps {
@@ -16,7 +15,7 @@ const Todo: React.FC<TodoProps> = ({ id, name, done, onChange = () => {}, onDele
       <input className="todo__checkbox" type="checkbox" checked={done} onChange={() => onChange(id)} />
       <span className="todo__text">{name}</span>
       <button className="todo__delete" onClick={() => onDelete(id)}>
-        <i className="fas fa-trash" />
+        delete
       </button>
     </div>
   )
