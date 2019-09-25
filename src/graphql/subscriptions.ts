374ea4f842c21b4cd5272b0d3c345ce2dd0269ab
suzukalight <mail@mkubara.com>
2019-09-25 19:21:14 +0900

    Run amplify add api and push

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,27 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    name
+    done
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    name
+    done
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    name
+    done
+  }
+}
+`;
